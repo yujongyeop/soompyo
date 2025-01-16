070c48d34c4b4bea76540ec1bd84030ff9a0f383
Platform1117 <230233@naver.com>
2025-01-17 00:31:45 +0900

    chore #1 : Spring 프로젝트 설정정

---
+++ server/src/main/java/com/jongyeop/soompyo/SoompyoApplication.java
@@ -0,0 +1,13 @@
+package com.jongyeop.soompyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoompyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoompyoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/jongyeop/soompyo/SoompyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongyeop.soompyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoompyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
