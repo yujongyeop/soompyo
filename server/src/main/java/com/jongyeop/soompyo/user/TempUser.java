25ba9a237c22370173bdf9c121498bbc1ebdd1ed
jongyeob <230233@naver.com>
2025-03-22 22:01:27 +0900

    feat #6 : Add Diary Entity
    ## 개요
    일기 엔티티를 추가하였습니다.
    이때 사용자 엔티티를 통해 일기를 조회할 수 있게 OneToMany 어노테이션을 추가했습니다.
    또한, 기존 사용자의 PK명이 모호하였던 부분을 수정할 필요성을 느껴 수정했습니다.
    
    ### 내용
    - 일기 엔티티 추가
    - TempUser 엔티티 id 필드의 컬럼명 변경 및 생성 방식 변경(AUTO -> IDENTITY)
    - TempUser 엔티티에 diaries 필드 추가

--- server/src/main/java/com/jongyeop/soompyo/user/TempUser.java
+++ server/src/main/java/com/jongyeop/soompyo/user/TempUser.java
@@ -1,15 +1,27 @@
 package com.jongyeop.soompyo.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jongyeop.soompyo.diary.model.Diary;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class TempUser {
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_uuid")
 	private Long id;
 	private String username;
 	private String userId;
+	@OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+	private List<Diary> diaries = new ArrayList<>();
 
 	protected TempUser() {
 
