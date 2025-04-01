e8a1daa5b703412947a32b788f16c46b240c88ef
jongyeob <230233@naver.com>
2025-04-01 21:14:33 +0900

    Refactor #6: Improve Diary Save Logic
    [개요]
    - Lombok 추가로 빌더 패턴을 사용하는 방식으로 변경 필요
    - 그에 따른 내부 로직 변경 및 편의 메소드 추가
    
    [수정사항]
    - Diary와 DiaryDto에 빌더 패턴 적용
    - toEntity와 toDto 편의 메소드 정의
    - 편의 메소드 정의에 따른 컨트롤러와 서비스 로직 일부 변경
    
    [결과]
    - 컨트롤러와 서비스 계층 역할 분리
    - 코드 간소화
    - Entity <-> Dto 변환 편의성 제공
    
    [연관 이슈]
    #7, #4

--- server/src/main/java/com/jongyeop/soompyo/diary/model/Diary.java
+++ server/src/main/java/com/jongyeop/soompyo/diary/model/Diary.java
@@ -3,6 +3,7 @@ package com.jongyeop.soompyo.diary.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.jongyeop.soompyo.diary.dto.DiaryDto;
 import com.jongyeop.soompyo.user.model.TempUser;
 
 import jakarta.persistence.Column;
@@ -14,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Diary {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,5 +39,28 @@ public class Diary {
 	private LocalDate targetDate;
 	private LocalDateTime createdDate;
 	private LocalDateTime modifiedDate;
+
+	public static Diary toEntity(DiaryDto dto) {
+		return Diary.builder()
+			.id(dto.getId())
+			.title(dto.getTitle())
+			.content(dto.getContent())
+			.targetDate(dto.getTargetDate())
+			.createdDate(dto.getCreatedDate())
+			.modifiedDate(dto.getModifiedDate())
+			.build();
+	}
+
+	public static Diary toEntity(DiaryDto dto, TempUser owner) {
+		return Diary.builder()
+			.id(dto.getId())
+			.owner(owner)
+			.title(dto.getTitle())
+			.content(dto.getContent())
+			.targetDate(dto.getTargetDate())
+			.createdDate(dto.getCreatedDate())
+			.modifiedDate(dto.getModifiedDate())
+			.build();
+	}
 
 }
