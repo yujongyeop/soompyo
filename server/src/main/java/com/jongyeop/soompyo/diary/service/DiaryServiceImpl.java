b2142fc5fb5951d857ff6926529ece53e3c855fe
Platform1117 <230233@naver.com>
2025-05-01 11:36:35 +0900

    Fix: 누락된 Import문 추가

--- server/src/main/java/com/jongyeop/soompyo/diary/service/DiaryServiceImpl.java
+++ server/src/main/java/com/jongyeop/soompyo/diary/service/DiaryServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
